1b43ccfbecce8b1d02db0280d789218887cae4b5
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-06-09 12:41:00 +0200

    Instalado Tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('tailwind', './assets/styles/tailwind.css')
+    .enablePostCssLoader()
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
