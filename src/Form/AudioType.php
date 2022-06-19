d5a0b8c9c7ce8f4bcd02435f9b1a6d3b2b999464
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-06-20 00:58:39 +0200

    Formulario embebido de audio

--- src/Form/AudioType.php
+++ src/Form/AudioType.php
@@ -4,22 +4,27 @@ namespace App\Form;
 
 use App\Entity\Audio;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
+use App\Form\RecursoType;
 
 class AudioType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('duracion')
-            ->add('recurso')
+            ->add('recurso', RecursoType::class)
+            ->add('duracion', TextType::class)
             ->add('audioFile', VichFileType::class, [
                 'required' => false,
                 'allow_delete' => false,
-                'download_label' => '...',
-                'download_uri' => true
+                'download_label' => '',
+                'download_uri' => false,
+                'attr' => array(
+                    'class' => 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'
+                ),
             ])
         ;
     }
