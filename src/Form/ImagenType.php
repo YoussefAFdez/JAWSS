d6a9370ea229686220c854af3ac87add91a3737c
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-06-20 00:08:23 +0200

    Desactivada URI de Download

--- src/Form/ImagenType.php
+++ src/Form/ImagenType.php
@@ -24,7 +24,7 @@ class ImagenType extends AbstractType
                 'required' => false,
                 'allow_delete' => false,
                 'download_label' => '',
-                'download_uri' => true,
+                'download_uri' => false,
                 'attr' => array(
                     'class' => 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'
                 )
