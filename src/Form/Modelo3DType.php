f5a5dd8a5774b36a705ff06ca096b6e2a09d9a8a
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-06-20 01:28:47 +0200

    Formulario Embebido de Modelo3D

--- src/Form/Modelo3DType.php
+++ src/Form/Modelo3DType.php
@@ -4,27 +4,45 @@ namespace App\Form;
 
 use App\Entity\Modelo3D;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
+use App\Form\RecursoType;
 
 class Modelo3DType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('material')
-            ->add('relleno')
-            ->add('resolucion')
-            ->add('soportes')
-            ->add('url')
-            ->add('recurso')
+            ->add('recurso', RecursoType::class)
+            ->add('material', TextType::class, [
+                'label' => 'Material:'
+            ])
+            ->add('relleno', NumberType::class, [
+                'label' => 'Relleno:'
+            ])
+            ->add('resolucion', TextType::class, [
+                'label' => 'Resolución'
+            ])
+            ->add('soportes', CheckboxType::class, [
+                'label' => 'Soportes',
+                'required' => false,
+            ])
+            ->add('url', TextType::class, [
+                'label' => 'URL: '
+            ])
             ->add('modeloFile', VichFileType::class, [
-                'label' => 'Fichero',
+                'label' => 'Subir fichero:',
                 'required' => false,
                 'allow_delete' => false,
-                'download_label' => '...',
-                'download_uri' => true,
+                'download_label' => '',
+                'download_uri' => false,
+                'attr' => array(
+                    'class' => 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'
+                ),
             ])
         ;
     }
