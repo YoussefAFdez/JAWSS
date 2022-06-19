11d73d0eda3d97b129654881cd8fc4bd81a2ab0f
YoussefAFdez <youssef.a.fdez@gmail.com>
2022-06-20 00:22:22 +0200

    Creado Fomulario Embebido

--- src/Form/VideoType.php
+++ src/Form/VideoType.php
@@ -7,17 +7,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
+use App\Entity\Recurso;
+use App\Form\RecursoType;
 
 class VideoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('recurso', RecursoType::class)
             ->add('duracion')
-            ->add('recurso')
             ->add('videoFile', VichFileType::class, [
+                'label' => 'Subir fichero:',
                 'required' => false,
-                'allow_delete' => false
+                'allow_delete' => false,
+                'attr' => array(
+                    'class' => 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'
+                ),
+                'download_uri' => false
             ])
         ;
     }
